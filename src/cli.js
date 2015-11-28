e2124025f8d1901460928f1f9012f766f726c477
Andy Fleming <andy@andyfleming.com>
2015-11-28 03:07:08 -0800

    cli experiments

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,19 @@
-import program from 'commander'
-import setupCommand from './cli-commands/setup'
+process.argv.forEach(function(val, index, array) {
+    console.log(index + ': ' + val);
+});
 
-program
-    .command('setup', 'runs setup assistant')
-    .action(setupCommand)
-
-program.parse(process.argv)
+//import program from 'commander'
+//import setupCommand from './cli/commands/setup'
+//
+//console.log('loaded');
+//
+//console.log(process.argv.length);
+//
+//program
+//    .version('0.0.1')
+//    .command('setup', 'runs setup assistant')
+//    .action(setupCommand)
+//
+//program.parse(process.argv)
+//
+//console.log('post exec');
