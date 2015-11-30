6fb111ce7d844f04a6ecfa3212b9f815190dabd3
Andy Fleming <andy@andyfleming.com>
2015-11-29 17:34:31 -0800

    Adding mysql (knex) connection

---
+++ src/db/connection.js
@@ -0,0 +1,23 @@
+let dotenv = require('dotenv')
+let knex = require('knex')
+
+// Load environment variables from .env file
+dotenv.load()
+
+let connection = knex({
+    client: 'mysql2',
+    connection: {
+        host     : process.env.DB_HOST,
+        user     : process.env.DB_USER,
+        password : process.env.DB_PASS,
+        database : process.env.DB_NAME,
+        port     : process.env.DB_PORT
+    }/*,
+    pool: {
+        min: 0,
+        max: 7
+    }*/
+});
+
+
+module.exports = connection
