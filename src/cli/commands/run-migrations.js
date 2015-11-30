17f808c40f428a9c9aecca01ef8e1b1da387d2dd
Andy Fleming <andy@andyfleming.com>
2015-11-29 17:34:49 -0800

    Splitting commands to setup, migrate, and create_migration

---
+++ src/cli/commands/run-migrations.js
@@ -0,0 +1,13 @@
+let migrationRunner = require('../../db/migration-runner')
+
+
+module.exports = (cmd, options) => {
+
+    console.log('Running migrations')
+    process.exit(0)
+
+    //migrationRunner(() => {
+    //    process.exit(0);
+    //});
+
+}
--- src/cli/commands/setup.js
+++ src/cli/commands/setup.js
@@ -1,12 +1,14 @@
 let promptly = require('promptly')
+let migrationRunner = require('../../db/migration-runner')
 
 module.exports = (cmd, options) => {
-
-    console.log('Setup command')
 
     promptly.prompt('Name: ', function (err, value) {
         // err is always null in this case, because no validators are set
         console.log(value);
+        migrationRunner(() => {
+            process.exit(0);
+        });
     });
 
 };
