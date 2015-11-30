43a52ecd766b7a6d43fbd652b6d51e22c8ee1709
Andy Fleming <andy@andyfleming.com>
2015-11-30 04:52:07 -0800

    Re-enabling migrations

--- src/cli/commands/run-migrations.js
+++ src/cli/commands/run-migrations.js
@@ -4,10 +4,9 @@ let migrationRunner = require('../../db/migration-runner')
 module.exports = (cmd, options) => {
 
     console.log('Running migrations')
-    process.exit(0)
 
-    //migrationRunner(() => {
-    //    process.exit(0);
-    //});
+    migrationRunner(() => {
+        process.exit(0);
+    });
 
 }
