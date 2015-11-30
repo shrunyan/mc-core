2eaf6f721dc8d15c9a39cdb23decb93d3ef1bb0a
Andy Fleming <andy@andyfleming.com>
2015-11-30 04:52:19 -0800

    Adding password field

--- src/db/migrations/20151129171720_create-users-table.js
+++ src/db/migrations/20151129171720_create-users-table.js
@@ -3,6 +3,7 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable('users', function (table) {
         table.increments()
         table.string('email')
+        table.string('password')
         table.string('first_name')
         table.string('last_name')
         table.timestamp('created_at').nullable()
