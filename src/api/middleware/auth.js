3c23c2324f73dcca21d5b27042cad422cd9127e7
Andy Fleming <andy@andyfleming.com>
2015-11-30 14:22:38 -0800

    Cleanup

--- src/api/middleware/auth.js
+++ src/api/middleware/auth.js
@@ -3,19 +3,17 @@ let jwt = require('jsonwebtoken');
 module.exports = function authMiddleware(req, res, next) {
   console.log('Auth Middleware: Start')
 
-  // TODO: check for JWT token
-  console.log(req.cookies);
+  // check for JWT token
   if (req.cookies.mc_jwt) {
 
     try {
-      console.log('attempting to verify JWT')
-
       var decoded = jwt.verify(req.cookies.mc_jwt, process.env.SECRET_KEY)
 
       // TODO: look up user
       req.user = {
         id: 1,
-        first_name: 'Andy'
+        first_name: 'Andy',
+        is_admin: 1
       }
       next()
 
--- ui/assets/js/helpers/auth-interceptor.js
+++ ui/assets/js/helpers/auth-interceptor.js
@@ -1,11 +1,5 @@
 export default ['$q', '$rootScope', function($q, $rootScope) {
   return {
-    //'response': function(response) {
-    //  if (response.status == 401) {
-    //    console.log('found 401!');
-    //  }
-    //  return response;
-    //}
     'responseError': function(response) {
 
       // If this is a login attempt, return the response for login controller to handle
@@ -15,9 +9,7 @@ export default ['$q', '$rootScope', function($q, $rootScope) {
 
       // Otherwise, if this is a 401, prompt re-authentication
       if (response.status == 401) {
-
         return $rootScope.showLogin(response);
-
       }
 
       // If it is not a 401 and not a login attempt, just return the error for the controller to handle
