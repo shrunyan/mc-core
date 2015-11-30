1f811b5e1e9ee85f765b757c5c0ec46f7ec42958
Andy Fleming <andy@andyfleming.com>
2015-11-29 21:11:09 -0800

    updating properties on user table

--- src/db/migrations/20151129171720_create-users-table.js
+++ src/db/migrations/20151129171720_create-users-table.js
@@ -1,9 +1,11 @@
 
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('users', function (table) {
-        table.increments();
-        table.string('name');
-        table.timestamp('created_at');
+        table.increments()
+        table.string('email')
+        table.string('first_name')
+        table.string('last_name')
+        table.timestamp('created_at')
         table.timestamp('updated_at')
     })
 };
