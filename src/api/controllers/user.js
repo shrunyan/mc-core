3778f0c9843ad22d3ad03cbc0b972d97ddb455b3
Andy Fleming <andy@andyfleming.com>
2015-11-30 02:51:19 -0800

    Starting user controller

---
+++ src/api/controllers/user.js
@@ -0,0 +1,17 @@
+modules.exports = {
+
+  getUser: function(req, res) {
+
+  },
+
+  login: function(req, res) {
+
+    // verify username and password
+
+    // if successful, respond with JWT
+
+    // otherwise, respond with 401
+
+  }
+
+}
