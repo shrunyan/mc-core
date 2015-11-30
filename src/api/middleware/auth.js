bf88e75f68dc49bf8bee01efb01d8bc6e10c4594
Andy Fleming <andy@andyfleming.com>
2015-11-30 02:51:28 -0800

    Rewriting auth middleware

--- src/api/middleware/auth.js
+++ src/api/middleware/auth.js
@@ -1,35 +1,7 @@
-import passport from 'passport'
-import {Strategy} from 'passport-local'
-
-passport.use(new Strategy(function (username, pasword, done) {
-  // console.log('HERE')
-  return done(null, false)
-}))
-// passport.serializeUser(serialize)
-// passport.deserializeUser(deSerialize)
-
-// function verify (username, pasword, done) {
-//   // TODO: fetch user from database
-//   console.log('HERE')
-//   return done(null, false)
-// }
-
-export function serialize (user, cb) {
-  cb(null, user.id)
-}
-
-export function deSerialize (id, cb) {
-  // TODO: use ID to look in DB
-  // and return user
-}
-
-export default function middleware (req, res, next) {
+modules.exports = function authMiddleware(req, res, next) {
   console.log('Auth Middleware: Start')
 
-  // Auth this biz
-  passport.authenticate('local', {
-    failureRedirect: '/fail'
-  })
+  // TODO: check for JWT token
 
   console.log('Auth Middleware: Done')
 
