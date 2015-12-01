b99c6782bc340b8de8676cb655e9c66e8b106ee4
Andy Fleming <andy@andyfleming.com>
2015-11-30 17:49:33 -0800

    cleanup

--- src/queueing/jobs-queue.js
+++ src/queueing/jobs-queue.js
@@ -1,9 +1,9 @@
 let RedisSMQ = require('rsmq');
-let rsmq = new RedisSMQ( {host: "127.0.0.1", port: 6379, ns: "mission_control"} );
+let rsmq = new RedisSMQ( {host: process.env.REDIS_HOST, port: process.env.REDIS_PORT, ns: 'mission_control'} );
 
-rsmq.createQueue({qname:"jobs"}, function (err, resp) {
+rsmq.createQueue({qname:'jobs'}, function (err, resp) {
   if (resp===1) {
-    console.log("queue created")
+    //console.log('queue created')
   }
 });
 
