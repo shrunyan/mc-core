d9c3a5d35e8c28ce821195cc4eb7bca5690ef95c
Andy Fleming <andy@andyfleming.com>
2015-12-02 00:31:40 -0800

    Abstracting out not-found route, adding TODOs, and cleaning up

--- src/api/routes.js
+++ src/api/routes.js
@@ -4,78 +4,34 @@ let express = require('express')
 let authMiddleware = require('./middleware/auth')
 let pipelinesController = require('./controllers/pipelines')
 let userController = require('./controllers/user')
+let notFoundController = require('./controllers/not-found')
 
 module.exports = function (app) {
 
   // Authentication middleware
   app.use('/api/*', authMiddleware)
 
-  app.get('/test-queue', function (req, res) {
-
-    let jobsRsmq = require('../queueing/jobs-queue')
-    jobsRsmq.sendMessage({qname: 'jobs', message: 'Hello World'}, function (err, resp) {
-
-      if (resp) {
-
-        console.log('Message sent. ID:', resp)
-
-      }
-
-      if (err) {
-
-        res.status(500).send({message: 'An error occurred'})
-
-      }
-
-    })
-
-    res.send({message: 'Item added to queue'})
-
-  })
-
   // User
   app.get('/api/user', userController.getUser)
   app.post('/login', userController.login)
   app.all('/logout', userController.logout)
 
+  // Groups
+  // TODO: GET /api/groups
+  // TODO: POST / api/groups
+
   // Pipelines
   app.get('/api/pipelines', pipelinesController.getList)
+  // TODO: POST /api/pipelines
 
-  // app.get('/', (req, res) => {
-  //  res.send({message: 'Welcome to the API!'})
-  // })
-  //
-  // app.use('*', function(req, res) {
-  //  res.sendFile('index.html')
-  // })
+  // Pipeline Executions
+  // TODO: GET /api/pipeline-executions
+  // TODO: POST /api/pipeline-executions
 
   // Static files
   app.use(express.static('./ui-build/'))
 
   // 404
-  app.use(function (req, res, next) {
-
-    res.status(404)
-
-    // respond with html page
-    if (req.accepts('html')) {
-
-      res.send('<h1>404 Not found</h1>')
-      return
-
-    }
-
-    // respond with json
-    if (req.accepts('json')) {
-
-      res.send({error: 'Not found'})
-      return
-
-    }
-
-    // default to plain-text. send()
-    res.type('txt').send('Not found')
-
-  })
+  app.use(notFoundController.notFound)
 
 }
