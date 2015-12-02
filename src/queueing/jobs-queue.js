af0b7ee7e673714915a608fe69ab5f5dbec57a3e
Andy Fleming <andy@andyfleming.com>
2015-12-01 17:00:00 -0800

    Starting to re-add padding

--- src/queueing/jobs-queue.js
+++ src/queueing/jobs-queue.js
@@ -2,12 +2,14 @@ let RedisSMQ = require('rsmq')
 let rsmq = new RedisSMQ({host: process.env.REDIS_HOST, port: process.env.REDIS_PORT, ns: 'mission_control'})
 
 rsmq.createQueue({qname: 'jobs'}, function (err, resp) {
+
   // If queue is successfully created
   if (resp === 1) {
   }
   if (err) {
     throw err
   }
+
 })
 
 module.exports = rsmq
--- ui/assets/js/view-controllers/login-controller.js
+++ ui/assets/js/view-controllers/login-controller.js
@@ -1,7 +1,9 @@
 export default ['$rootScope', '$http', '$q', function ($rootScope, $http, $q) {
+
   $rootScope.showLoginError = false
 
   $rootScope.showLogin = function showLogin (originalResponse) {
+
     jQuery('#login').fadeIn()
 
     let deferred = $q.defer()
@@ -9,26 +11,38 @@ export default ['$rootScope', '$http', '$q', function ($rootScope, $http, $q) {
     $rootScope.promise = deferred
 
     return deferred.promise
+
   }
 
   $rootScope.attemptLogin = function attemptLogin () {
+
     let data = {
       email: $rootScope.loginFormEmail,
       password: $rootScope.loginFormPassword
     }
 
     $http.post('/login', data).then(function (response) {
+
       $rootScope.promise.resolve($http($rootScope.originalResponse.config))
       jQuery('#login').hide()
       $rootScope.showLoginError = false
+
     }, function () {
+
       $rootScope.showLoginError = true
+
     })
+
   }
 
   $rootScope.logout = function logout () {
+
     $http.post('/logout').then(function () {
+
       $rootScope.showLogin()
+
     })
+
   }
+
 }]
