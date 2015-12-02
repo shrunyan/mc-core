e8939e854d81514917c9f3cd7549875105ce1a4e
Stuart Runyan <shrunyan@gmail.com>
2015-12-01 19:10:07 -0800

    Finish setup flow. Checks .env variables, mysql & redis

--- src/cli/README.md
+++ src/cli/README.md
@@ -2,8 +2,11 @@
 
 From inside this package you can run `npm link` to setup the `mc` executable. This gives commands that will help you configure your .env, MySql and Redis instances.
 
-Available Commands:
+## Getting Started
 
+Run `npm link` to install peer dependencies and connect cli tool to project `node_modules` directory.
+
+## Available Commands
+  
+  $mc --help
   $mc setup --help
-  $mc migration --help
-  $mc run --help
--- src/cli/index.js
+++ src/cli/index.js
@@ -13,17 +13,25 @@ fs.access(ENV_FILE, function (err) {
   if (err) {
     console.log(colors.red('We could not load your .env file.'))
     console.log(colors.yellow('Check these requirements before proceding.'))
+
+    // .env instructions
     console.log(colors.white('1) .env file exists'))
     console.log(colors.grey('Use the .env.example file to get started.'))
+
+    // Redis instructions
     console.log(colors.white('2) Redis is installed and running'))
-    console.log(colors.white('3) MySql is installed and running'))
+    console.log(colors.grey('If running locally on OSX use brew to install and start Redis.'))
+
+    // MySQL instructions
+    console.log(colors.white('3) MySQL is installed and running'))
+    console.log(colors.grey('If running locally on OSX use brew to install and start MySQL'))
   } else {
     program
       .version(pkg.version)
       .usage('<command> [options]')
       .command('setup', 'runs setup assistant')
-      .command('migration', 'make a new migration')
-      .command('run', 'run migrations')
+      // .command('migration', 'make a new migration')
+      // .command('run', 'run migrations')
       .parse(process.argv)
   }
 })
