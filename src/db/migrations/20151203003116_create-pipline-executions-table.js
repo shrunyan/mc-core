ffa8d9ab5e6cdc2977f83db778d5c6ac8953ed22
Andy Fleming <andy@andyfleming.com>
2015-12-03 00:40:10 -0800

    Noting potential values

--- src/db/migrations/20151203003116_create-pipline-executions-table.js
+++ src/db/migrations/20151203003116_create-pipline-executions-table.js
@@ -5,7 +5,7 @@ exports.up = function (knex, Promise) {
   return knex.schema.createTable('pipeline_executions', function (table) {
     table.increments()
     table.integer('pipeline_id')
-    table.string('status')
+    table.string('status') // Should be one of "queued", "running", "failed", "succeeded"
     table.timestamp('started_at').nullable()
     table.timestamp('finished_at').nullable()
     table.timestamp('created_at').nullable()
