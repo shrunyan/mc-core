effbb021fbdcbd3abf04dc5db265687f0f6aebff
Stuart Runyan <shrunyan@gmail.com>
2015-12-02 20:46:04 -0800

    WIP: Projects listing

--- ui/assets/js/controllers/projects.js
+++ ui/assets/js/controllers/projects.js
@@ -2,6 +2,7 @@ export default ['$scope', '$http', function ($scope, $http) {
   $http.get('/api/projects').then(function (response) {
 
     // TODO: Get response from api
+    // TODO: Map data and derive url friendly paths from names
     $scope.projects = [{
       id: 1,
       name: 'Project one',
