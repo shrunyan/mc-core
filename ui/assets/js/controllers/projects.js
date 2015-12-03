c78bfc0e85f9de7850bb598ec31ddd07b410f091
Stuart Runyan <shrunyan@gmail.com>
2015-12-02 20:02:25 -0800

    Organize templates

---
+++ ui/assets/js/controllers/projects.js
@@ -0,0 +1,17 @@
+export default ['$scope', '$http', function ($scope, $http) {
+  $http.get('/api/projects').then(function (response) {
+
+    // TODO: Get response from api
+    $scope.projects = [{
+      id: 1,
+      name: 'Project one',
+      path: 'project-one',
+      pipelines: [{
+        id: 1,
+        name: 'Pipeline one',
+        path: 'pipeline-one'
+      }]
+    }]
+
+  })
+}]
