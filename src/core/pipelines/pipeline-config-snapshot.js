b70c4e3a144877e5de86ca636688b7bfc5e5a236
Andy Fleming <andy@andyfleming.com>
2015-12-06 01:34:03 -0800

    Starting on pipeline configuration snapshot generator module

---
+++ src/core/pipelines/pipeline-config-snapshot.js
@@ -0,0 +1,25 @@
+'use strict'
+
+module.exports = {
+
+  /**
+   * Returns an aggregate object for a full pipeline configuration snapshot. Includes:
+   * - initial variable values
+   * - stages
+   * - stage configuration values
+   *
+   * Ultimately, this is used to execute a pipeline and its stages
+   *
+   * @param id
+   */
+  load: id => {
+
+    // TODO: load pipeline
+
+    // inject initial variables value
+
+    // TODO: inject stages (with config)
+
+  }
+
+}
