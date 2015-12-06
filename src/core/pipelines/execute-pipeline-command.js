d196446bb9bb1f47568fdffe502c4d065c1e47db
Andy Fleming <andy@andyfleming.com>
2015-12-06 05:05:49 -0800

    Work on actually creating record and queueing

--- src/core/pipelines/execute-pipeline-command.js
+++ src/core/pipelines/execute-pipeline-command.js
@@ -1,31 +1,62 @@
 'use strict'
 
+let connection = require('../../db/connection')
+let logger = require('tracer').colorConsole()
 let pipelineRsmq = require('../../queueing/pipeline-queue')
+let snapshotBuilder = require('./config-snapshot-builder')
 
-module.exports = (pipelineId, input) => {
+module.exports = (pipelineId, input, callback) => {
 
   input = input || []
 
   // TODO: snapshot pipeline configuration
+  let snapshot = snapshotBuilder(pipelineId)
 
-  // TODO: create pipeline execution entry in DB
+  // TODO: validate input (and fill in defaults if necessary)
 
-  // TODO: send message to queue to handle new execution
-  let message = JSON.stringify({
-    pipeline_execution_id: 1
-  })
-  pipelineRsmq.sendMessage({qname: 'pipeline_executions', message: message}, function (err, resp) {
+  let newExecutionData = {
+    pipeline_id: pipelineId,
+    status: 'created',
+    started_at: new Date(),
+    updated_at: new Date(),
+    initial_values: JSON.stringify(input),
+    config_snapshot: JSON.stringify(snapshot)
+  }
 
-    if (resp) {
+  // Create pipeline execution entry in DB
+  connection.insert(newExecutionData, 'id').into('pipeline_executions').then((id) => {
 
-      console.log('Message sent. ID:', resp)
+    let newExecutionId = id[0]
 
-    }
+    callback(newExecutionId)
 
-    if (err) {
-      throw err
-    }
+    // Send message to queue to handle new execution
+    let message = JSON.stringify({
+      pipeline_execution_id: newExecutionId
+    })
+    pipelineRsmq.sendMessage({qname: 'pipeline_executions', message: message}, function (err, resp) {
 
+      if (resp) {
+        console.log('Message sent. ID:', resp)
+        // Mark new execution as queued (using the id captured above)
+        connection('pipeline_executions')
+          .where('id', newExecutionId)
+          .update('status','queued').catch(err => {
+            logger.error(err)
+          })
+
+      }
+
+      if (err) {
+        logger.error(err)
+        throw err
+      }
+
+    })
+
+  }).catch(err => {
+    logger.error(err)
+    throw err
   })
 
 }
