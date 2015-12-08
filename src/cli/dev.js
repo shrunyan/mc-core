da321e0ccddd3112b0e053603c2b7309eb559b1e
Andy Fleming <andy@andyfleming.com>
2015-12-08 03:35:58 -0800

    Enabling events

--- src/cli/dev.js
+++ src/cli/dev.js
@@ -13,16 +13,16 @@ module.exports = () => {
         ext: 'js json'
     })
 
-    //nodemon.on('start', function () {
-    //    console.log('App has started')
-    //
-    //}).on('quit', function () {
-    //    console.log('App has quit')
-    //
-    //}).on('restart', function (files) {
-    //    console.log('App restarted due to: ', files)
-    //
-    //})
-    
+    nodemon.on('start', function () {
+        console.log('App has started')
+
+    }).on('quit', function () {
+        console.log('App has quit')
+
+    }).on('restart', function (files) {
+        console.log('App restarted due to: ', files)
+
+    })
+
 }
 
