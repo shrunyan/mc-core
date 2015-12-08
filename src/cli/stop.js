efe5dc48a6f863af6c23bdf43dbe6016bf511bd4
Andy Fleming <andy@andyfleming.com>
2015-12-08 04:27:10 -0800

    Building stop command

--- src/cli/stop.js
+++ src/cli/stop.js
@@ -1,6 +1,7 @@
 'use strict'
 
 var pm2 = require('pm2');
+let processes = require('./processes')
 
 module.exports = (args) => {
 
@@ -8,14 +9,30 @@ module.exports = (args) => {
     // Connect or launch PM2
     pm2.connect(function(err) {
 
-        // Start a script on the current folder
-        pm2.delete('mission-control-web', function(err, proc) {
-            if (err) throw new Error('err');
+        let promises = []
 
-            // Disconnect to PM2
-            pm2.disconnect(function() { process.exit(0) });
+        processes.forEach(config => {
 
-        });
+            promises.push(new Promise((resolve, reject) => {
+
+                pm2.delete(config.name, function(err, proc) {
+                    if (err) throw new Error('err');
+
+                    // Disconnect to PM2
+                    pm2.disconnect(function() {
+                        resolve()
+                    });
+
+                });
+            }))
+
+        })
+
+        // When all managed pm2 processes have been deleted, exit this one
+        Promise.all(promises).then(() => {
+            process.exit(0)
+        })
+
     })
 
 }
