87b127fcb1f10643c9b89c6f771277105baa2b6d
Andy Fleming <andy@andyfleming.com>
2015-12-08 04:23:50 -0800

    Configuring for multiple processes

---
+++ src/cli/processes.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = [
+    {
+        name: 'mc-web',
+        script: 'node_modules/mc-core/src/server.js'
+    },
+    {
+        name: 'mc--pipelines-worker',
+        script: 'node_modules/mc-core/src/worker-pipelines.js'
+    }
+]
--- src/cli/start.js
+++ src/cli/start.js
@@ -1,31 +1,33 @@
 'use strict'
 
-var pm2 = require('pm2');
+let pm2 = require('pm2');
+let processes = require('./processes')
 
 module.exports = (args) => {
 
     let devMode = (args.length > 1 && args[1] === '--dev');
 
-    // node_modules/mc-core/src/server.js
-    // node_modules/mc-core/src/worker-pipelines.js
-
+    // If we are in dev mode, add additional options
+    if (devMode) {
+        processes.forEach((proccesConfig, index) => {
+            processes[index].watch = ["node_modules/mc-core"]
+            processes[index].ignore_watch = ["node_modules/mc-core/node_modules"]
+        })
+    }
     // Connect or launch PM2
     pm2.connect(function(err) {
 
-        let webProcessConfig = {
-            name: 'mission-control-web',
-            "watch": ["node_modules/mc-core"],
-            "ignore_watch" : ["node_modules/mc-core/node_modules"]
-        }
-
-        // Start a script on the current folder
-        pm2.start('node_modules/mc-core/src/server.js', webProcessConfig, function(err, proc) {
+        // Start our processes
+        pm2.start(processes, function(err, proc) {
             if (err) throw new Error('err');
 
-            // Disconnect to PM2
-            pm2.disconnect(function() { process.exit(0) });
+            // Disconnect to PM2 (and exit this script after)
+            pm2.disconnect(function() {
+                process.exit(0)
+            });
 
         });
+
     })
 
 }
