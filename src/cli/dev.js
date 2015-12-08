d7f21e8ad01be8dbadde2f8d089e68e24debfb85
Andy Fleming <andy@andyfleming.com>
2015-12-08 03:17:02 -0800

    Running server using nodemon

--- src/cli/dev.js
+++ src/cli/dev.js
@@ -1,16 +1,28 @@
 'use strict'
 
-//var nodemon = require('nodemon');
-//
-//nodemon({
-//    script: 'app.js',
-//    ext: 'js json'
-//});
-//
-//nodemon.on('start', function () {
-//    console.log('App has started');
-//}).on('quit', function () {
-//    console.log('App has quit');
-//}).on('restart', function (files) {
-//    console.log('App restarted due to: ', files);
-//});
+var nodemon = require('nodemon');
+
+module.exports = () => {
+
+    /*
+     * Run server
+     */
+
+    nodemon({
+        script: 'node_modules/mc-core/src/server.js',
+        ext: 'js json'
+    })
+
+    //nodemon.on('start', function () {
+    //    console.log('App has started')
+    //
+    //}).on('quit', function () {
+    //    console.log('App has quit')
+    //
+    //}).on('restart', function (files) {
+    //    console.log('App restarted due to: ', files)
+    //
+    //})
+    
+}
+
