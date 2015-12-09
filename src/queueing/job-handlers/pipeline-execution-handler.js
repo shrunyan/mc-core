07ccf7438ef07e3cb803af4ef93aca94e467dcd3
Andy Fleming <andy@andyfleming.com>
2015-12-09 12:24:32 -0800

    Setting pipeline metadata on start and finish

--- src/queueing/job-handlers/pipeline-execution-handler.js
+++ src/queueing/job-handlers/pipeline-execution-handler.js
@@ -1,18 +1,50 @@
 'use strict'
 
+let connection = require('../../db/connection')
+let logger = require('tracer').colorConsole()
+
 module.exports = (msg, next, msgid) => {
 
-    // process your message
-    logger.debug('message received. ID: ' + msgid)
-    logger.debug(msg)
+  // process your message
+  logger.debug('message received. ID: ' + msgid)
+  logger.debug(msg)
 
-    // TODO: validate the incoming job data
+  // Parse the JSON in the message
+  msg = JSON.parse(msg)
 
-    // TODO: look up the pipeline execution
-    
-    // TODO: mark the pipeline as successful
+  // Validate the incoming job data
+  if (!msg.pipeline_execution_id) {
+    logger.error('a queue job was submitted to pipeline_executions, but with no pipeline_execution_id')
+  }
 
-    // Mark the queue job as complete, and move onto the next
-    next()
+  // TODO: look up the pipeline execution
+
+  // Mark the pipeline execution as started
+  connection('pipeline_executions')
+    .where('id', msg.pipeline_execution_id)
+    .update({
+      status: 'running',
+      started_at: new Date(),
+      updated_at: new Date()
+    }).then(() => {
+
+    // Mark the pipeline_execution as successful
+    connection('pipeline_executions')
+      .where('id', msg.pipeline_execution_id)
+      .update({
+        status: 'succeeded',
+        finished_at: new Date(),
+        updated_at: new Date()
+      }).then(() => {
+
+      // TODO: emit event for pipeline_execution update
+
+      // Mark the queue job as complete, and move onto the next
+      next()
+
+    })
+
+  })
+
 
 }
