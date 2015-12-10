c3388dd6ea67eb78713441582a555f81672dab29
Andy Fleming <andy@andyfleming.com>
2015-12-10 02:34:32 -0800

    Starting test fixture

--- src/db/connection.js
+++ src/db/connection.js
@@ -6,15 +6,22 @@ let knex = require('knex')
 // Load environment variables from .env file
 dotenv.load()
 
-let connection = knex({
-  client: 'mysql2',
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
-  }
-})
+let connection
+
+if (process.env.NODE_ENV === 'test') {
+  connection = {}
+} else {
+  connection = knex({
+    client: 'mysql2',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT
+    }
+  })
+}
+
 
 module.exports = connection
