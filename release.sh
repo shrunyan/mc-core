907853b34522cc8b353f42d82f5db29d5e0550ec
Andy Fleming <andy@andyfleming.com>
2015-12-09 17:32:24 -0800

    More notes and organizing for release script

--- release.sh
+++ release.sh
@@ -3,21 +3,39 @@
 # Usage
 # ./release.sh 1.0.0
 
+####################################################################################################
+# Validate Environment, User, Git Repo condition before do any actual work
+####################################################################################################
+
 # Exit if no argument (version) was passed
 if [ "$#" -ne 1 ]; then
     echo "release requires 1 argument: version number (ex. 1.0.0)"
     exit 0
 fi
 
+# TODO: Check that the npm user logged in
+# See npm whoami;echo $?
+
+# TODO: Check that the npm user is someone who is authorized to release
+# See npm whoami
+
 # TODO: change package.json version or ensure that it is updated
+# ???
 
 # TODO: ensure our remote is the main repo
+# See git config --get remote.origin.url
+# Should equal git@github.com:space-race/mc-core.git
 
 # TODO: exit if any uncommitted changes
 # See git diff --exit-code --quiet;echo $?
 
 # TODO: ensure we are on the develop branch
 # See git rev-parse --abbrev-ref HEAD
+
+
+####################################################################################################
+# Start Actual Work
+####################################################################################################
 
 # lock package versions
 npm shrinkwrap
