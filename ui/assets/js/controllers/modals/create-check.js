8f34cea22d889d33aa5c9c9c7414b0bd2b77814b
Stuart Runyan <shrunyan@gmail.com>
2015-12-11 13:46:19 -0800

    Update create-check modal controller to use the new checks api

--- ui/assets/js/controllers/modals/create-check.js
+++ ui/assets/js/controllers/modals/create-check.js
@@ -2,11 +2,12 @@ export default ['$scope', '$http', '$uibModalInstance', function ($scope, $http,
 
   $scope.ok = () => {
     $http
-      .post('/api/projects', {
+      .post('/api/check', {
         project_id: $scope.$parent.$id,
         name: $scope.name
       })
       .then(res => {
+        console.log('create check response', res)
         $uibModalInstance.close(res.data.data)
       })
   }
