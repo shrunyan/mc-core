3ef73c1f3a6178d8cab2097e5627cbde52101c65
Stuart Runyan <shrunyan@gmail.com>
2015-12-11 11:54:22 -0800

    Add create health check modal template and controller

--- ui/assets/js/controllers/health.js
+++ ui/assets/js/controllers/health.js
@@ -1,15 +1,17 @@
 export default ['$scope', '$http', function ($scope, $http) {
 
-  $http.get('/api/projects').then(function(response) {
-    let projects = response.data.data
+  $http
+    .get('/api/projects')
+    .then((res) => {
+      let projects = res.data.data
 
-    $scope.projectHalves = [[], []]
+      $scope.projectHalves = [[], []]
 
-    // Split the projects into two arrays so we can output them in two columns elegantly
-    projects.forEach((project, index) => {
-      $scope.projectHalves[(index % 2 === 0) ? 0 : 1].push(project)
+      // Split the projects into two arrays so we can output them in two columns elegantly
+      projects.forEach((project, index) => {
+        $scope.projectHalves[(index % 2 === 0) ? 0 : 1].push(project)
+      })
+
     })
-
-  })
 
 }]
--- ui/assets/js/controllers/modals.js
+++ ui/assets/js/controllers/modals.js
@@ -1,10 +1,37 @@
-
 import createPipelineModal from './modals/create-pipeline'
 import createProjectModal from './modals/create-project'
+import createCheckModal from './modals/create-check'
 
 export default ['$rootScope', '$uibModal', ($rootScope, $uibModal) => {
 
   $rootScope.modal = {
+
+    open: function open (modal, cb) {
+      $uibModal
+        .open(this.getConfig(modal))
+        .result
+        .then(cb)
+    },
+
+    getConfig: function getConfig (modal) {
+      switch (modal) {
+        case 'project':
+          return {
+            templateUrl: '/assets/js/templates/modals/create-project.html',
+            controller: createProjectModal
+          }
+        case 'pipeline':
+          return {
+            templateUrl: '/assets/js/templates/modals/create-pipeline.html',
+            controller: createPipelineModal
+          }
+        case 'check':
+          return {
+            templateUrl: '/assets/js/templates/modals/create-check.html',
+            controller: createCheckModal
+          }
+      }
+    },
 
     createProject: function createProject (cb) {
       let modal = $uibModal.open({
