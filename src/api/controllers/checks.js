726bb8d34b50552060ce2fa376b257962a543089
Stuart Runyan <shrunyan@gmail.com>
2015-12-11 17:01:45 -0800

    WIP: creating health checks

--- src/api/controllers/checks.js
+++ src/api/controllers/checks.js
@@ -4,13 +4,8 @@ let logger = require('tracer').colorConsole()
 let basic = require('./basic-response-helper')
 
 module.exports = {
-
   create: (req, res) => basic.insertRespond(req, res, 'health_checks'),
-
-  getAll: (req, res) => {},
-
+  getAll: (req, res) => basic.getList(req, res, 'health_checks'),
   getCheck: (req, res) => {},
-
   getByProject: (req, res) => {}
-
 }
--- ui/assets/js/controllers/health.js
+++ ui/assets/js/controllers/health.js
@@ -1,16 +1,20 @@
 export default ['$scope', '$http', function ($scope, $http) {
 
   $http
-    .get('/api/projects')
-    .then((res) => {
-      let projects = res.data.data
+    .get('/api/checks')
+    .then(res => {
 
-      $scope.projectHalves = [[], []]
+      let checks = res.data.data
 
-      // Split the projects into two arrays so we can output them in two columns elegantly
-      projects.forEach((project, index) => {
-        $scope.projectHalves[(index % 2 === 0) ? 0 : 1].push(project)
-      })
+      console.log('health checks', checks)
+
+
+      // $scope.projectHalves = [[], []]
+
+      // // Split the projects into two arrays so we can output them in two columns elegantly
+      // checks.forEach((project, index) => {
+      //   $scope.projectHalves[(index % 2 === 0) ? 0 : 1].push(project)
+      // })
 
     })
 
