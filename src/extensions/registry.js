c6eff4a1f634ae306cd0a2fb5d57a6e85a62cdd9
Andy Fleming <andy@andyfleming.com>
2015-12-14 11:35:49 -0800

    Starting on implementation of registry

--- src/extensions/registry.js
+++ src/extensions/registry.js
@@ -1,5 +1,59 @@
 'use strict'
 
+let fs = require('fs')
+let logger = require('tracer').colorConsole()
+let validator = require('mc-extension-validar')
+
+let registry = {
+
+  _registeredExtensions: {},
+
+  loadFrom: (dir) => {
+    // Loop through extensions found in node_modules/ (anything starting with mc-ext-)
+    fs.readdirSync(dir, (err, files) => {
+      files.forEach((file) => {
+        if (file.startsWith('mc-ext-')) {
+          let extensionToRegister = require(file)
+          this.register(extensionToRegister, file)
+        }
+      })
+    })
+  },
+
+  validate: (ext) => {
+    validator.validateIndex(ext, (errors, warnings) => {
+
+    })
+  },
+
+  register: (ext, file) => {
+    this.validate(ext, () => {
+        this._registeredExtensions[ext.vendor][ext.name] = ext
+    })
+  },
+
+  getType: (extensionItemPath) => {
+    let parts = extensionItemPath.split('.')
+    let path = {
+      vendor: parts[0],
+      name: parts[1],
+      type: parts[2],
+      typeName: parts[3]
+    }
+
+    return this._registeredExtensions[path.vendor][path.name][path.type][path.typeName]
+  }
+
+}
+
+registry.loadFrom('node_modules')
+
+//registry.getType('mc.core.stage.pause-for-x-seconds')
+
+module.exports = registry
+
+
+
 // Loop through node_modules directory
 // Look for some sort of indicator that a node_module is a mission control extension
 // - mc-extension.js
@@ -7,19 +61,18 @@
 // - mc-extension.json (in addition to index.js)
 // - package is named mc-ext-*
 
+
+
 // If it is an mc extension, "load it"
-let ext = require('module')
+//let ext = require('module')
+//
+//registerExtension(ext)
 
-registerExtension(ext)
-
-
-
-module.exports = registry
 
 
 // During pipeline execution
-registry.get('mc.core.stage.pause')
-registry.getStageTypes()
+//registry.get('mc.core.stage.pause')
+//registry.getStageTypes()
 
 
 // TODO: need to register extensions on startup
@@ -37,30 +90,3 @@ registry.getStageTypes()
 // Find any ones with mc-extension.js
 // If so newExt = ../../node_modules/example/mc-extension
 // registry[newExt.vendor][newExt.name] = newExt
-
-let registry = {
-
-  registeredExtensions: {},
-
-  register: (ext) => {
-    this.registeredExtensions[ext.vendor][ext.name] = ext
-  },
-
-  getType: (extensionItemPath) => {
-    let parts = extensionItemPath.split('.')
-    let path = {
-      vendor: parts[0],
-      name: parts[1],
-      type: parts[2],
-      typeName: parts[3]
-    }
-
-    return this.registeredExtensions[path.vendor][path.name][path.type][path.typeName]
-  }
-
-}
-
-registry.getType('mc.core.stage.pause-for-x-seconds')
-
-module.exports = registry
-
