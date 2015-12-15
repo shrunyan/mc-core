a47d52e49cf67926b501100b7b86a53d9f9d623d
Stuart Runyan <shrunyan@gmail.com>
2015-12-15 10:47:19 -0800

    Fix tests after resgistry module changes

---
+++ test/unit/spec.registry.js
@@ -0,0 +1,51 @@
+var test = require('tape')
+var sinon = require('sinon')
+var extensions = require('../../src/extensions/registry')
+
+const MODULE_STUB = {
+  vendor: 'test',
+  name: 'test'
+}
+
+test('load()', function (t) {
+  var spy = sinon.spy(extensions, 'resolve')
+
+  extensions.load()
+
+  t.ok(spy.calledOnce, 'invokes resolve()')
+  t.end()
+})
+
+test('resolve()', function (t) {
+  var spy = sinon.spy(extensions, 'register')
+
+  extensions.resolve(process.cwd() + '/node_modules/sinon')
+
+  t.ok(spy.called, 'invokes register()')
+
+  spy.restore()
+
+  t.end()
+})
+
+test('register() ', function (t) {
+  extensions._extensions = {}
+  extensions.register(MODULE_STUB)
+
+  t.ok(extensions._extensions.test.test, 'set module to internal _extensions prop')
+  t.end()
+})
+
+test('validate()', function (t) {
+  t.skip(false, 'uses validator module')
+  t.end()
+})
+
+test('get()', function (t) {
+  t.ok(extensions.get('test.test'), 'resolves extension path')
+  t.throws(() => {
+    extensions.get('wrong.test')
+  }, 'throws path error for undefined extension paths')
+  t.end()
+})
+
