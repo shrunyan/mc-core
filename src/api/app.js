2961641c7fde427801eebd227a59acdb9e5301dd
Andy Fleming <andy@andyfleming.com>
2015-12-14 18:20:02 -0800

    Splitting out app from server

---
+++ src/api/app.js
@@ -0,0 +1,26 @@
+'use strict'
+
+let fs = require('fs')
+let dotenv = require('dotenv')
+let express = require('express')
+let session = require('express-session')
+let cookieParser = require('cookie-parser')
+let bodyParser = require('body-parser')
+let morgan = require('morgan')
+let routes = require('./routes')
+
+dotenv.config({silent: true})
+
+let app = express()
+
+app.use(morgan('dev'))
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+// Load routes
+routes(app)
+
+module.exports = app
--- src/server.js
+++ src/server.js
@@ -1,29 +1,9 @@
 'use strict'
 
-let fs = require('fs')
-let dotenv = require('dotenv')
-let express = require('express')
-let session = require('express-session')
-let cookieParser = require('cookie-parser')
-let bodyParser = require('body-parser')
-let morgan = require('morgan')
-let routes = require('./api/routes')
-
-dotenv.config({silent: true})
+let app = require('./api/app')
 
 let port = process.env.PORT || 3000
 let host = process.env.HOST || 'localhost'
-let app = express()
-
-app.use(morgan('dev'))
-app.use(cookieParser())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
-
-// Load routes
-routes(app)
 
 let server = app.listen(port, () => {
 
