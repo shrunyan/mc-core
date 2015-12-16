c8a46ab4b25406b0dbada3fbdf0843a1c3ef0dd0
Andy Fleming <andy@andyfleming.com>
2015-12-16 14:14:03 -0800

    Loading real project name

--- ui/assets/js/controllers/pipeline.js
+++ ui/assets/js/controllers/pipeline.js
@@ -1,7 +1,12 @@
 export default ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams) {
 
-  $http.get('/api/pipelines/' + $stateParams.id).then(function(response) {
+  $http.get('/api/pipelines/' + $stateParams.id).then(response => {
     $scope.pipeline = response.data.data
+
+    $http.get('/api/projects/'+ response.data.data.project_id).then(response => {
+      $scope.project = response.data.data
+    })
+
   })
 
 }]
