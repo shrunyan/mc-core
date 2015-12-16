0b289c416fcfdcd25624dc4f10b8cee8a9887df4
Andy Fleming <andy@andyfleming.com>
2015-12-16 02:35:30 -0800

    Switching order

--- src/core/pipelines/get-active-pipelines.js
+++ src/core/pipelines/get-active-pipelines.js
@@ -5,7 +5,7 @@ let connection = require('../../db/connection')
 module.exports = (callback) => {
 
   connection.select()
-    .orderBy('finished_at', 'desc')
+    .orderBy('created_at', 'asc')
     .whereNull('finished_at')
     .from('pipeline_executions')
     .then((rows) => {
