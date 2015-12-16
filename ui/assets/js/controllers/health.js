6d273d5161aa0ef4a1be9014d1f62eca33263c27
Stuart Runyan <shrunyan@gmail.com>
2015-12-16 12:40:01 -0800

    Render checks fetched from api

--- ui/assets/js/controllers/health.js
+++ ui/assets/js/controllers/health.js
@@ -4,17 +4,8 @@ export default ['$scope', '$http', function($scope, $http) {
     .get('/api/checks')
     .then(res => {
 
-      let checks = res.data.data
-
-      console.log('health checks', checks)
-
-
-      // $scope.projectHalves = [[], []]
-
-      // // Split the projects into two arrays so we can output them in two columns elegantly
-      // checks.forEach((project, index) => {
-      //   $scope.projectHalves[(index % 2 === 0) ? 0 : 1].push(project)
-      // })
+      console.log('health checks', res.data.data)
+      $scope.checks = res.data.data
 
     })
 
