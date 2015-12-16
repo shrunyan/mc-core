307a2158ea146d1ef07f581604183217c031aa83
Andy Fleming <andy@andyfleming.com>
2015-12-16 14:10:42 -0800

    Formatting

--- ui/assets/js/controllers/pipeline.js
+++ ui/assets/js/controllers/pipeline.js
@@ -1,6 +1,6 @@
 export default ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams) {
 
-  $http.get('/api/pipelines/'+$stateParams.id).then(function(response) {
+  $http.get('/api/pipelines/' + $stateParams.id).then(function(response) {
     $scope.pipeline = response.data.data
   })
 
