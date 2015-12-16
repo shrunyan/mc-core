5d7189756ce4f02f0917842a1af5a2174b091487
Andy Fleming <andy@andyfleming.com>
2015-12-15 17:00:48 -0800

    Removing export

--- src/server.js
+++ src/server.js
@@ -5,11 +5,10 @@ let app = require('./api/app')
 let port = process.env.PORT || 3000
 let host = process.env.HOST || 'localhost'
 
-let server = app.listen(port, () => {
+app.listen(port, () => {
 
   console.log('Mission Control listening at http://%s:%s', host, port)
   console.log('app locals', app.locals.ext)
 
 })
 
-module.exports = server
