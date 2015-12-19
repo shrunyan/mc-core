68bae210b8cbce01becd1b48a62fcdbd659a7a85
Stuart Runyan <shrunyan@gmail.com>
2015-12-19 12:56:33 -0800

    WIP: save sage config

--- src/api/controllers/stages.js
+++ src/api/controllers/stages.js
@@ -1,16 +1,28 @@
 'use strict'
 
-let pkgs = require('../../extensions/registry').load()
+let registry = require('../../extensions/registry')
 
 module.exports = {
+  createStageConfig: function createStageConfig(req, res) {
+    const STAGE_ID = req.body.stage_id
+    const STAGE = registry.get(STAGE_ID)
+
+    console.log('stage', STAGE)
+
+  },
   getAvailableTypes: function getAvailableTypes(req, res) {
+    let pkgs = registry.load()
     let stages = []
 
     for (let vendor in pkgs) {
       for (let ext in pkgs[vendor]) {
         for (let stage in pkgs[vendor][ext].stages) {
+          let vendorName = pkgs[vendor][ext].vendor
+          let extName = pkgs[vendor][ext].name
+          let stageName = pkgs[vendor][ext].stages[stage].name
+
           stages.push({
-            id: [vendor, ext, stage].join('.'),
+            id: [vendorName, extName, stageName].join('.'),
             name: stage
           })
         }
