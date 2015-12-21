99a942cc25de5d47f6cf90861bdadb501bf4ec2b
Andy Fleming <andy@andyfleming.com>
2015-12-21 05:26:54 -0800

    Simplifying loading data

--- src/api/controllers/stages.js
+++ src/api/controllers/stages.js
@@ -1,25 +1,14 @@
 'use strict'
 
-let pkgs = require('../../extensions/registry').load()
+let registry = require('../../extensions/registry')
 let basic = require('./basic-response-helper')
+
+registry.load()
 
 module.exports = {
 
   getAvailableTypes: function getAvailableTypes(req, res) {
-    let stages = []
-
-    for (let vendor in pkgs) {
-      for (let ext in pkgs[vendor]) {
-        for (let stage in pkgs[vendor][ext].stages) {
-          stages.push({
-            id: [vendor, ext, stage].join('.'),
-            name: stage
-          })
-        }
-      }
-    }
-
-    res.status(201).send({data: stages})
+    res.status(201).send({data: registry.getStageTypes()})
   },
 
   /**
