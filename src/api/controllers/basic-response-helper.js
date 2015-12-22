1bd0c6e76836408db66c33ae32cbade6d90ec4ba
Andy Fleming <andy@andyfleming.com>
2015-12-22 12:59:59 -0800

    Protecting id field and adding updated_at

--- src/api/controllers/basic-response-helper.js
+++ src/api/controllers/basic-response-helper.js
@@ -73,11 +73,22 @@ module.exports = {
    * @param table
    */
   patchRespond: (req, res, table) => {
+
+    let changes = req.body
+
+    // Don't allow ID to be changed
+    if (typeof changes.id !== 'undefined') {
+      delete changes.id
+    }
+
+    // Append a updated_at date
+    changes.updated_at = new Date()
+
     if (req.params.id) {
       connection
         .table(table)
         .where('id', req.params.id)
-        .update(req.body)
+        .update(changes)
         .then(item => {
           logger.log(item)
           res.status(200).send({message: 'Updated: ' + req.params.id})
