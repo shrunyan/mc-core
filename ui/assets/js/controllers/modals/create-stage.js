1e0c9a370565bdccf45bfc4d8b6080901c526006
Stuart Runyan <shrunyan@gmail.com>
2015-12-21 21:33:25 -0800

    Create stage config from modal

--- ui/assets/js/controllers/modals/create-stage.js
+++ ui/assets/js/controllers/modals/create-stage.js
@@ -1,18 +1,30 @@
 export default ['$scope', '$http', '$uibModalInstance', function($scope, $http, $uibModalInstance) {
 
-  console.log('scope', $scope)
+  // get stages in parent controller
+  console.log('create-stage scope', $scope.$parent.$stages)
 
   $http
     .get('/api/stage-types')
     .then(res => $scope.stages = res.data.data)
 
   $scope.ok = () => {
+
+    let sort = $scope.$parent.$stages
+      ? $scope.$parent.$stages.length + 1
+      : 0;
+
     $http
-      .post('api/stages/', {
-        stage_id: $scope.stage_id
+      .post('/api/stage/config', {
+        pipeline_config_id: $scope.$parent.$id,
+        type: $scope.stage_id,
+        sort: sort,
+        // options: {}
       })
       .then(res => {
-        console.log('res', res)
+        console.log(res)
+      })
+      .catch(err => {
+        console.error(err)
       })
 
     $uibModalInstance.close()
