df6a7f9e2e4fd1a2d23904e73dc209c2c483afdd
Stuart Runyan <shrunyan@gmail.com>
2015-12-23 11:20:41 -0800

    Organize code, using fat arrow funcs to make the code read a little more terse

--- src/worker-pipelines.js
+++ src/worker-pipelines.js
@@ -1,33 +1,21 @@
 'use strict'
 
 let RSMQWorker = require('rsmq-worker')
-let dotenv = require('dotenv')
+let dotenv = require('dotenv').load()
 let logger = require('tracer').colorConsole()
 let jobHandler = require('./queueing/job-handlers/pipeline-execution-handler')
-
-console.log('Starting worker for pipelines')
-
-dotenv.load()
-
 let worker = new RSMQWorker('pipeline_executions', {
   redisPrefix: 'mission_control',
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT
 })
 
+logger.log('WORKER: Pipeline worker started')
 worker.on('message', jobHandler)
 
 // optional error listeners
-worker.on('error', function(err, msg) {
-  logger.error('ERROR', err, msg.id)
-})
-
-worker.on('exceeded', function(msg) {
-  logger.error('EXCEEDED', msg.id)
-})
-
-worker.on('timeout', function(msg) {
-  logger.error('TIMEOUT', msg.id, msg.rc)
-})
+worker.on('error', (err, msg) => logger.error('ERROR', err, msg))
+worker.on('exceeded', msg => logger.error('EXCEEDED', msg))
+worker.on('timeout', msg => logger.error('TIMEOUT', msg))
 
 worker.start()
