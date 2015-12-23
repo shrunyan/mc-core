92c3c70298ea045d8d3ee789af4ea4d2b6a886c8
Stuart Runyan <shrunyan@gmail.com>
2015-12-23 11:13:49 -0800

    Ensure pipeline execution is not attempted unless an id is provided

--- src/queueing/job-handlers/pipeline-execution-handler.js
+++ src/queueing/job-handlers/pipeline-execution-handler.js
@@ -12,7 +12,8 @@ module.exports = (msg, next) => {
   // Validate the incoming job data
   if (!msg.pipeline_execution_id) {
     logger.error('a queue job was submitted to pipeline_executions, but with no pipeline_execution_id')
+  } else {
+    new PipelineExecutor(msg.pipeline_execution_id, next)
   }
 
-  new PipelineExecutor(msg.pipeline_execution_id, next)
 }
