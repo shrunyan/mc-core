a52b6fe776a2e9631fa527e94ac1cbe866b536c1
Andy Fleming <andy@andyfleming.com>
2015-12-26 21:02:14 -0500

    Documenting blocks

--- ui/assets/js/controllers/configure-pipeline.js
+++ ui/assets/js/controllers/configure-pipeline.js
@@ -10,6 +10,7 @@ export default ['$q', '$scope', '$http', '$stateParams', '$state', function($q, 
     saved: {}
   }
 
+  // Load the pipeline and the related project
   $http.get('/api/pipelines/' + $stateParams.id).then(response => {
     $scope.pipeline = response.data.data
 
@@ -19,6 +20,7 @@ export default ['$q', '$scope', '$http', '$stateParams', '$state', function($q, 
 
   })
 
+  // Load the pipeline variables
   $http.get('/api/pipelines/' + $stateParams.id + '/variables').then(response => {
     $scope.variables = response.data.data
   })
