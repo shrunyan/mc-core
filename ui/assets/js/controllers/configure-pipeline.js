001e84847e7946dae6a6e6c048c3798df34ebe96
Andy Fleming <andy@andyfleming.com>
2015-12-26 21:21:36 -0500

    Reorganizing into functions

--- ui/assets/js/controllers/configure-pipeline.js
+++ ui/assets/js/controllers/configure-pipeline.js
@@ -11,80 +11,87 @@ export default ['$q', '$scope', '$http', '$stateParams', '$state', function($q, 
   }
 
   // Load the pipeline and the related project
-  $http.get('/api/pipelines/' + $stateParams.id).then(response => {
-    $scope.pipeline = response.data.data
+  $scope.loadPipelinesAndProject = function loadPipelinesAndProject() {
+    $http.get('/api/pipelines/' + $stateParams.id).then(response => {
+      $scope.pipeline = response.data.data
 
-    $http.get('/api/projects/' + response.data.data.project_id).then(response => {
-      $scope.project = response.data.data
-    })
-
-  })
-
-  // Load the pipeline variables
-  $http.get('/api/pipelines/' + $stateParams.id + '/variables').then(response => {
-    $scope.variables = response.data.data
-  })
-
-  // Get Stage Data
-  let stageTypes = $http.get('/api/stage-types')
-  let stageConfigs = $http.get('/api/pipelines/' + $stateParams.id + '/stages')
-  $q.all([stageTypes, stageConfigs])
-    .then(args => {
-      let types = args[0].data.data
-      let stages = args[1].data.data
-
-      // Join the types with their stages
-      $scope.stages = stages.map(stage => {
-        let type = types.find(type => type.fqid === stage.type)
-        if (type) {
-          stage.schema = type
-        }
-        return stage
+      $http.get('/api/projects/' + response.data.data.project_id).then(response => {
+        $scope.project = response.data.data
       })
 
-      // Copy the options for stages to two maps
-      stages.forEach(function(stage) {
+    })
+  }
 
-        console.log(stage)
+  // Load the pipeline variables
+  $scope.loadVariables = function loadVariables() {
+    $http.get('/api/pipelines/' + $stateParams.id + '/variables').then(response => {
+      $scope.variables = response.data.data
+    })
+  }
 
-        // Set up holder objects
-        // - one for the "last saved" value
-        // - one for the current (bound) form value
-        // - one for metadata
-        $scope.stageOptions.current[stage.id] = {}
-        $scope.stageOptions.saved[stage.id] = {}
-        $scope.stageOptions.metadata[stage.id] = {}
+  // Get Stage Data
+  $scope.loadStages = function loadStages() {
 
-        // For each option that exits in the stage type...
-        Object.keys(stage.schema.options).forEach((key) => {
+    let stageTypes = $http.get('/api/stage-types')
+    let stageConfigs = $http.get('/api/pipelines/' + $stateParams.id + '/stages')
+    $q.all([stageTypes, stageConfigs])
+      .then(args => {
+        let types = args[0].data.data
+        let stages = args[1].data.data
 
-          let optionValue
-
-          // If the user has specified value, use that
-          // Otherwise, if there is a default value, use it
-          // Otherwise, make the value blank
-          if (typeof stage.options[key] !== 'undefined') {
-            optionValue = stage.options[key].toString()
-
-          } else if (typeof stage.schema.options[key].default !== 'undefined') {
-            optionValue = stage.schema.options[key].default.toString()
-
-          } else {
-            optionValue = ''
+        // Join the types with their stages
+        $scope.stages = stages.map(stage => {
+          let type = types.find(type => type.fqid === stage.type)
+          if (type) {
+            stage.schema = type
           }
+          return stage
+        })
 
-          $scope.stageOptions.current[stage.id][key] = optionValue
-          $scope.stageOptions.saved[stage.id][key] = optionValue
-          $scope.stageOptions.metadata[stage.id][key] = {
-            name: stage.schema.options[key].name,
-            description: stage.schema.options[key].description
-          }
+        // Copy the options for stages to two maps
+        stages.forEach(function(stage) {
+
+          console.log(stage)
+
+          // Set up holder objects
+          // - one for the "last saved" value
+          // - one for the current (bound) form value
+          // - one for metadata
+          $scope.stageOptions.current[stage.id] = {}
+          $scope.stageOptions.saved[stage.id] = {}
+          $scope.stageOptions.metadata[stage.id] = {}
+
+          // For each option that exits in the stage type...
+          Object.keys(stage.schema.options).forEach((key) => {
+
+            let optionValue
+
+            // If the user has specified value, use that
+            // Otherwise, if there is a default value, use it
+            // Otherwise, make the value blank
+            if (typeof stage.options[key] !== 'undefined') {
+              optionValue = stage.options[key].toString()
+
+            } else if (typeof stage.schema.options[key].default !== 'undefined') {
+              optionValue = stage.schema.options[key].default.toString()
+
+            } else {
+              optionValue = ''
+            }
+
+            $scope.stageOptions.current[stage.id][key] = optionValue
+            $scope.stageOptions.saved[stage.id][key] = optionValue
+            $scope.stageOptions.metadata[stage.id][key] = {
+              name: stage.schema.options[key].name,
+              description: stage.schema.options[key].description
+            }
+
+          })
 
         })
 
       })
-
-    })
+  }
 
   $scope.saveOptions = function saveOptions(stageId) {
 
@@ -105,7 +112,7 @@ export default ['$q', '$scope', '$http', '$stateParams', '$state', function($q, 
   // Delete Stage
   $scope.removeStage = (id) => {
     $http.delete('/api/stage/' + id)
-    $state.go($state.$current, null, { reload: true })
+    $scope.loadStages()
   }
 
   $scope.createVar = () => {}
@@ -126,5 +133,10 @@ export default ['$q', '$scope', '$http', '$stateParams', '$state', function($q, 
     }
 
   }
+
+  // Initialize view
+  $scope.loadStages()
+  $scope.loadPipelinesAndProject()
+  $scope.loadVariables()
 
 }]
