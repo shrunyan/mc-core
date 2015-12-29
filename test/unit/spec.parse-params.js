4db0a5abc9536467e0df071104d276ecb7d81276
Andy Fleming <andy@andyfleming.com>
2015-12-29 12:58:58 -0800

    Implementing test

--- test/unit/spec.parse-params.js
+++ test/unit/spec.parse-params.js
@@ -1,8 +1,30 @@
+'use strict'
+
 var test = require('tape')
 var parseParams = require('../../src/api/controllers/util/parse-params')
 
 test('parseParams()', function(t) {
-  t.pass()
+
+  let params = {
+    a: 'wow',
+    b: '2',
+    c: {
+      sub: 'yup',
+      more: 'yeah'
+    },
+    d: {
+      even: {
+        deeper: '1'
+      }
+    }
+  }
+
+  let result = parseParams(params)
+
+  t.equal(result.a, 'wow')
+  t.equal(result.b, '2')
+  t.equal(result.c, '{"sub":"yup","more":"yeah"}')
+  t.equal(result.d, '{"even":{"deeper":"1"}}')
+
   t.end()
 })
-
