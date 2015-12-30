e735d98bb0066b065397b34963af2f73b5ed46c9
Andy Fleming <andy@andyfleming.com>
2015-12-30 04:21:37 -0800

    Noting unused file

--- src/core/pipelines/pipeline-executor.js
+++ src/core/pipelines/pipeline-executor.js
@@ -1,5 +1,12 @@
 'use strict'
 
+// DEPRECATED
+// -- BELOW LEFT FOR REFERENCE --
+
+
+
+
+
 let connection = require('../../db/connection')
 let logger = require('tracer').colorConsole()
 let extensionRegistry = require('../../extensions/registry')
