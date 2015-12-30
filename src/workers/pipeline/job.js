3b53c09a2607d82cf9dcdd70837d8b21a83b450f
Andy Fleming <andy@andyfleming.com>
2015-12-30 12:03:19 -0800

    Pipeline input/variable processing (and other pseudocode)

--- src/workers/pipeline/job.js
+++ src/workers/pipeline/job.js
@@ -27,12 +27,57 @@ module.exports = class Job {
     // Mark the pipeline as running
     Promise.resolve(this.pipeline.running())
 
-    // Get the initial pipeline data
+    // Make sure there is not unexpected input
+    for (let key in this.pipeline.input) {
+      if (typeof this.pipeline.config.variables[key] === 'undefined') {
+        // TODO: store a pipeline execution log noting that there was unexpected input
+        // TODO: mark the pipeline as failed (and complete)
+        return
+      }
+    }
+
+    let initialVariableValues
+
+    for (let key in this.pipeline.config.variables) {
+
+      // If the variable is a "required" "input"", make sure it exists (and capture it)
+      if (this.pipeline.config.variables[key].required) {
+        if (typeof this.pipeline.input[key] === 'undefined') {
+          // TODO: fail the pipeline
+          // TODO: mark the pipeline as failed (and complete)
+          return
+        }
+        initialVariableValues[key] = this.pipeline.input[key]
+
+
+      } else {
+        // Otherwise, if it is optional...
+
+        // Check if an input was provided. If so, use that, otherwise, use the default value
+        if (typeof this.pipeline.input[key] !== 'undefined') {
+          initialVariableValues[key] = this.pipeline.input[key]
+        } else {
+          initialVariableValues[key] = this.pipeline.config.variables[key].default_value
+        }
+
+      }
+    }
+
+    // this.pipeline.input
+    // merge the input with the default values
+    // ... in which case we could just capture input and use the config snapshot to apply the defaults...
+
+    // TODO: also, here we should validate that all of the required inputs were provided
+    // NOTE: I'm ok failing validation here. For example, if GitHub were to trigger a job without all of the required
+    // input, I would rather that the webhook succeeds and we can fail the job before it begins with a note saying
+    // why it wasn't run.
 
     // Load up the stages...
     // Set up a callback for when all the stages are complete...
     // Run each stage, and when the stage is done, move onto the next one...
     // ... unless it is the last one, in which case, call that callback and be done with the pipeline...
+
+    // Get the output from the stage and apply it to the pipeline variables as mapped
 
   }
 
