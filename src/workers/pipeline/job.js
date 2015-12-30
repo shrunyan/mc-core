54a8c671b605d4e4f927b9fccb9bf1ff92dd11e5
Andy Fleming <andy@andyfleming.com>
2015-12-30 14:50:08 -0800

    Refactoring variable prep into separate method

--- src/workers/pipeline/job.js
+++ src/workers/pipeline/job.js
@@ -17,89 +17,108 @@ module.exports = class Job {
    * @param {function} next Should be called on complete (to clear the message from the queue)
    */
   constructor(msg, next) {
-    this.next = next
     this.pipeline = new Pipeline(msg.id)
-    this.pipeline.load().then(() => this.run())
+    this.pipeline.load()
+      .then(this.pipeline.running())
+      .then(this.prepareInputAndVariables())
+      .then(this.run())
+      .then(this.pipeline.succeed())
+      .then(() => next())
+      .catch(err => logger.error(err))
+  }
+
+  /**
+   * Prepare initial values of variables
+   */
+  prepareInputAndVariables() {
+
+    return new Promise((resolve, reject) => {
+
+      // Validate the variables config snapshot value
+      if (!Array.isArray(this.pipeline.config.variables)) {
+        this.pipeline.config.variables = []
+      }
+
+      // Convert the variable objects into a list of expected input/variable keys
+      let expectedKeys = this.pipeline.config.variables.map(variable => variable.name)
+
+      // INPUT VALIDATION
+      // NOTE: I'm ok failing validation here. For example, if GitHub were to trigger a job without all of the required
+      // input, I would rather that the webhook succeed and we can fail the job here before it begins with a note saying
+      // it was missing a required input or provided input we didn't expect.
+
+      // Run through the user/trigger provided input (and make sure there is not unexpected input)
+      for (let key in this.pipeline.input) {
+
+        // If we can't find the input key in the expected input/variables...
+        if (expectedKeys.indexOf(key) === -1) {
+
+          // Note the event in the server logs
+          logger.warn('Input key "' + key + '" was provided but was not expected')
+
+          // Note the event in the pipeline execution logs
+          this.pipeline.log('Input key "' + key + '" was provided but was not expected')
+
+          // Fail the pipeline
+          this.pipeline.fail()
+          reject()
+          return
+        }
+      }
+
+      // Build the initial variable values by combining default values and input provided
+      // (also check if a required input is missing)
+      let initialVariableValues = {}
+
+      this.pipeline.config.variables.forEach(variable => {
+
+        let key = variable.name
+
+        // If the variable is a "required" "input"", make sure it exists (and capture it)
+        if (variable.required) {
+          if (typeof this.pipeline.input[key] === 'undefined') {
+
+            // Note the event in the server logs
+            logger.warn('Missing required input "' + key + '" for pipeline')
+
+            // Note the event in the pipeline execution logs
+            this.pipeline.log('Missing required input "' + key + '" for pipeline')
+
+            // Fail the pipeline
+            this.pipeline.fail()
+            reject()
+            return
+          }
+          initialVariableValues[key] = this.pipeline.input[key]
+
+        } else {
+          // Otherwise, if it is optional...
+
+          // Check if an input was provided. If so, use that, otherwise, use the default value
+          if (typeof this.pipeline.input[key] !== 'undefined') {
+            initialVariableValues[key] = this.pipeline.input[key]
+          } else {
+            initialVariableValues[key] = variable.default_value
+          }
+
+        }
+      })
+
+      logger.debug('initial variable values')
+      logger.debug(initialVariableValues)
+
+      this.initialVariableValues = initialVariableValues
+
+      resolve()
+
+    })
+
   }
 
   /**
    * Run the pipeline execution
    */
   run() {
-
-    // Mark the pipeline as running
-    Promise.resolve(this.pipeline.running())
-
-    // Validate the variables config snapshot value
-    if (!Array.isArray(this.pipeline.config.variables)) {
-      this.pipeline.config.variables = []
-    }
-
-    // Convert the variable objects into a list of expected input/variable keys
-    let expectedKeys = this.pipeline.config.variables.map(variable => variable.name)
-
-    // INPUT VALIDATION
-    // NOTE: I'm ok failing validation here. For example, if GitHub were to trigger a job without all of the required
-    // input, I would rather that the webhook succeed and we can fail the job here before it begins with a note saying
-    // it was missing a required input or provided input we didn't expect.
-
-    // Run through the user/trigger provided input (and make sure there is not unexpected input)
-    for (let key in this.pipeline.input) {
-
-      // If we can't find the input key in the expected input/variables...
-      if (expectedKeys.indexOf(key) === -1) {
-
-        // Note the event in the server logs
-        logger.warn('Input key "' + key + '" was provided but was not expected')
-
-        // Note the event in the pipeline execution logs
-        this.pipeline.log('Input key "' + key + '" was provided but was not expected')
-
-        // Fail the pipeline
-        this.pipeline.fail()
-        return
-      }
-    }
-
-    // Build the initial variable values by combining default values and input provided
-    // (also check if a required input is missing)
-    let initialVariableValues = {}
-
-    this.pipeline.config.variables.forEach(variable => {
-
-      let key = variable.name
-
-      // If the variable is a "required" "input"", make sure it exists (and capture it)
-      if (variable.required) {
-        if (typeof this.pipeline.input[key] === 'undefined') {
-
-          // Note the event in the server logs
-          logger.warn('Missing required input "' + key + '" for pipeline')
-
-          // Note the event in the pipeline execution logs
-          this.pipeline.log('Missing required input "' + key + '" for pipeline')
-
-          // Fail the pipeline
-          this.pipeline.fail()
-          return
-        }
-        initialVariableValues[key] = this.pipeline.input[key]
-
-      } else {
-        // Otherwise, if it is optional...
-
-        // Check if an input was provided. If so, use that, otherwise, use the default value
-        if (typeof this.pipeline.input[key] !== 'undefined') {
-          initialVariableValues[key] = this.pipeline.input[key]
-        } else {
-          initialVariableValues[key] = variable.default_value
-        }
-
-      }
-    })
-
-    logger.debug('initial variable values')
-    logger.debug(initialVariableValues)
 
     // Load up the stages...
     // Set up a callback for when all the stages are complete...
