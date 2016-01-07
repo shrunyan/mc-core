49b26da582394437e26fe0d67c8ddd071c12cbdf
Andy Fleming <andy@andyfleming.com>
2016-01-06 19:29:47 -0800

    Adding support for HTTPS (and WSS)

--- src/server.js
+++ src/server.js
@@ -1,13 +1,31 @@
 'use strict'
 
 let app = require('./api/app')
+let fs = require('fs')
 
 let port = process.env.PORT || 3000
 let host = process.env.HOST || 'localhost'
+let httpsOn = (typeof process.env.HTTPS === 'string' && process.env.HTTPS.toUpperCase() === 'ON')
+let server
 
-let server = app.listen(port, () => {
-  console.log('Mission Control listening at http://%s:%s', host, port)
-})
+if (httpsOn) {
+
+  const options = {
+    key: fs.readFileSync(process.env.HTTPS_KEY),
+    cert: fs.readFileSync(process.env.HTTPS_CERT)
+  }
+
+  server = require('https').createServer(options, app).listen(port, () => {
+    console.log('Mission Control listening at https://%s:%s', host, port)
+  })
+
+
+} else {
+  server = require('http').createServer(app).listen(port, () => {
+    console.log('Mission Control listening at http://%s:%s', host, port)
+  })
+
+}
 
 // Set up socket handling
 require('./api/socket-handler')(server)
