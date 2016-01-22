b2f18523c91ef987b4086a3825d3bacfd96816ae
Andy Fleming <andy@andyfleming.com>
2016-01-22 03:01:40 -0800

    Starting pipeline config duplication function

---
+++ src/core/pipelines/configure-pipeline-from-existing.js
@@ -0,0 +1,15 @@
+'use strict'
+
+module.exports = function(targetPipelineId, sourcePipelineId) {
+
+  return new Promise((resolve, reject) => {
+
+    // TODO: Look up stage configs for source pipeline
+
+    // TODO: Inject stage configs into target pipeline (with new pipeline id)
+
+    resolve()
+
+  })
+
+}
