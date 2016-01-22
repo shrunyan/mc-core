71011f82dd64a5c758744ba3f0335fdfcac20d54
Andy Fleming <andy@andyfleming.com>
2016-01-22 04:07:37 -0800

    Implementing copying of stages and variables

--- src/core/pipelines/configure-pipeline-from-existing.js
+++ src/core/pipelines/configure-pipeline-from-existing.js
@@ -1,18 +1,89 @@
 'use strict'
 
+let connection = require('../../db/connection')
+let logger = require('tracer').colorConsole()
+
 function copyStageConfigs(targetPipelineId, sourcePipelineId) {
-  // TODO: implement
+  return new Promise((resolve, reject) => {
+
+    connection
+      .table('pipeline_stage_configs')
+      .where('pipeline_config_id', sourcePipelineId)
+      .select()
+      .catch((err) => {
+        logger.error(err)
+        reject()
+      })
+      .then(items => {
+
+        if (items.length === 0) {
+          resolve()
+          return
+        }
+
+        items.forEach((item) => {
+          delete item.id
+          item.pipeline_config_id = targetPipelineId
+          item.created_at = item.updated_at = new Date()
+        })
+
+        connection
+          .insert(items)
+          .into('pipeline_stage_configs')
+          .catch((err) => {
+            logger.error(err)
+            reject()
+          })
+          .then(() => {
+            resolve()
+          })
+      })
+
+  })
 }
 
 function copyVariables(targetPipelineId, sourcePipelineId) {
-  // TODO: implement
+  return new Promise((resolve, reject) => {
+
+    connection
+      .table('pipeline_variables')
+      .where('pipeline_config_id', sourcePipelineId)
+      .select()
+      .catch((err) => {
+        logger.error(err)
+        reject()
+      })
+      .then(items => {
+
+        if (items.length === 0) {
+          resolve()
+          return
+        }
+
+        items.forEach((item) => {
+          delete item.id
+          item.pipeline_config_id = targetPipelineId
+          item.created_at = item.updated_at = new Date()
+        })
+
+        connection
+          .insert(items)
+          .into('pipeline_variables')
+          .catch((err) => {
+            logger.error(err)
+            reject()
+          })
+          .then(() => {
+            resolve()
+          })
+      })
+
+  })
 }
 
 module.exports = function(targetPipelineId, sourcePipelineId) {
-
   return Promise.all([
     copyStageConfigs(targetPipelineId, sourcePipelineId),
     copyVariables(targetPipelineId, sourcePipelineId)
   ])
-
 }
