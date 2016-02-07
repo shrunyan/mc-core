5fa5fa3a80e167c4aab66eed4c477293ac1bc9c9
Stuart Runyan <shrunyan@gmail.com>
2016-02-06 16:13:20 -0800

    Simplifies logic of tests

--- test/unit/spec.verify-jwt.js
+++ test/unit/spec.verify-jwt.js
@@ -12,105 +12,14 @@ test('verify jwt', (t) => {
   t.plan(1)
 
   verify(sign(TEST_USER_ID))
-    .then(decode => {
-      if (decoded.user_id === 'undefined') {
-        t.fail('decoded user_id not found')
-      }
-      if (decoded.user_id !== TEST_USER_ID) {
-        t.fail('decoded user_id does not match expectation')
-      }
-      t.pass()
-    })
-    .catch(() => {
-      t.fail('an error occurred validating and decoding the jwt')
-    })
+    .then(t.pass)
+    .catch(t.fail)
 })
 
-// test('test invalid jwt', function(t) {
-//   t.plan(1)
+test('test invalid jwt', (t) => {
+  t.plan(1)
 
-//   verify('fakejwt')
-//     .then((decoded) => {
-//       console.log('DECODED', decoded)
-//       t.fail('this should have been an invalid jwt')
-//     })
-//     .catch(() => {
-//       t.pass('the jwt was not validated')
-//     })
-// })
-
-
-
-
-
-
-// test('create, validate, and decode valid jwt', function(t) {
-
-//   t.plan(1)
-
-//   const TEST_USER_ID = 12
-
-//   let providedJwt = createJwt(TEST_USER_ID)
-
-//   validateAndDecodeJwt(providedJwt)
-//     .then((decoded) => {
-
-//       if (decoded.user_id === 'undefined') {
-//         t.fail('decoded user_id not found')
-//         return
-//       }
-
-//       if (decoded.user_id !== TEST_USER_ID) {
-//         t.fail('decoded user_id does not match expectation')
-//         return
-//       }
-
-//       t.pass()
-
-//     })
-//     .catch(() => {
-//       t.fail('an error occurred validating and decoding the jwt')
-//     })
-
-// })
-
-// test('test invalid jwt', function(t) {
-
-//   t.plan(1)
-
-//   validateAndDecodeJwt('fakejwt')
-//     .then((decoded) => {
-//       t.fail()
-
-//     })
-//     .catch(() => {
-//       t.pass('the jwt was not validated')
-//     })
-
-// })
-
-// test('test valid jwt with invalid alg', function(t) {
-
-//   t.plan(1)
-
-//   const TEST_USER_ID = 12
-//   let providedJwt
-
-//   try {
-//     // Testing with HS512 here. Not necessarily insecure, but rather testing that a single algorithm is enforced.
-//     providedJwt = jwt.sign({user_id: TEST_USER_ID}, process.env.SECRET_KEY, {algorithm: 'HS512'})
-
-//   } catch (err) {
-//     t.fail('JWT signing failed')
-//     return
-//   }
-
-//   validateAndDecodeJwt(providedJwt)
-//     .then(() => {
-//       t.fail('the jwt was validated when it shouldn\'t have been')
-//     })
-//     .catch(() => {
-//       t.pass('the jwt was not validated')
-//     })
-
-// })
+  verify('fakejwt')
+    .then(t.fail)
+    .catch(t.pass)
+})
